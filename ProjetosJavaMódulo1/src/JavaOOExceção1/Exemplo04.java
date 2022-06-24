d4c91a92210be623b99757cf8e1db4b2839cc551
Matheus Roberto Alves Da Silva <athalves@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-06-24 16:12:47 -0300

    Adicionando exemplo de exceção

---
+++ ProjetosJavaMódulo1/src/JavaOOExceção1/Exemplo04.java
@@ -0,0 +1,27 @@
+package JavaOOExceção1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exemplo04 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+
+        System.out.println("Digite um número inteiro:");
+
+        try {
+            numero = teclado.nextInt(); //Colocar letra para gerar exceção
+            System.out.println("Deu bom :)");
+            System.out.println(numero);
+        } catch(InputMismatchException ex) {
+            System.out.println("Deu ruim :(");
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        } finally {
+            System.out.println("Aqui sempre entra, não tem como correr ;)");
+        }
+
+
+    }
+}
