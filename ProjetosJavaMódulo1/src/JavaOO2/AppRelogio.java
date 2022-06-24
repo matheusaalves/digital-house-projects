56c18e7b944500c97507ec48c1a62fd793152f6f
Matheus Roberto Alves Da Silva <athalves@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-06-24 15:44:23 -0300

    Adicionando exemplo do relógio

---
+++ ProjetosJavaMódulo1/src/JavaOO2/AppRelogio.java
@@ -0,0 +1,13 @@
+package JavaOO2;
+
+public class AppRelogio {
+    public static void main(String[] args) {
+        Relogio relogio = new Relogio(12, 34, 11);
+
+        relogio.mostrarHoraCompleta();
+
+        relogio.setHora(25);
+
+        relogio.mostrarHoraCompleta();
+    }
+}
---
+++ ProjetosJavaMódulo1/src/JavaOO2/Relogio.java
@@ -0,0 +1,73 @@
+package JavaOO2;
+
+public class Relogio {
+    private int hora;
+    private int minuto;
+    private int segundo;
+
+    public Relogio(int hora, int minuto, int segundo) {
+        if (validaHora(hora)) {
+            this.hora = hora;
+        }
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+
+
+    public int getHora() {
+        return   hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+    public void setHora(int novaHora) {
+        if (validaHora(novaHora)) {
+            hora = novaHora;
+        }
+    }
+
+    public void setMinuto(int minuto) {
+        if(validaMinutoSegundo(minuto)) {
+            this.minuto = minuto;
+        }
+    }
+
+    public void setSegundo(int segundo) {
+        if(validaMinutoSegundo(segundo)) {
+            this.segundo = segundo;
+        }
+    }
+
+    public boolean validaHora(int hora) {
+        if(hora >= 0 && hora <= 24) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean validaMinutoSegundo(int tempo) {
+        if(tempo >= 0 && tempo <= 60) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void alterarMinuto(int novoMinuto) {
+        minuto = novoMinuto;
+    }
+
+    public void mostrarHoraCompleta() {
+        System.out.println(
+                hora + ":" +
+                minuto + ":" +
+                segundo);
+    }
+}
